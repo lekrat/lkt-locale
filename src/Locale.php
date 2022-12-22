d4a420387ae76fc0073608f601dbbdc73f1c02c2
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2022-12-22 16:18:48 +0100

    Move to PHP8.1

--- src/Locale.php
+++ src/Locale.php
@@ -7,38 +7,24 @@ use Lkt\Locale\Enums\LangCode;
 
 class Locale
 {
-    protected static $langCode = LangCode::English;
-    protected static $countryLangCode = CountryLangCode::EnglishFromGreatBritain;
+    protected static string $langCode = LangCode::English;
+    protected static string $countryLangCode = CountryLangCode::EnglishFromGreatBritain;
 
-    /**
-     * @param string $langCode
-     * @return void
-     */
-    public static function setLangCode(string $langCode)
+    public static function setLangCode(string $langCode): void
     {
         static::$langCode = $langCode;
     }
 
-    /**
-     * @return string
-     */
     public static function getLangCode(): string
     {
         return static::$langCode;
     }
 
-    /**
-     * @param string $langCode
-     * @return void
-     */
-    public static function setCountryLangCode(string $langCode)
+    public static function setCountryLangCode(string $langCode): void
     {
         static::$countryLangCode = $langCode;
     }
 
-    /**
-     * @return string
-     */
     public static function getCountryLangCode(): string
     {
         return static::$countryLangCode;
